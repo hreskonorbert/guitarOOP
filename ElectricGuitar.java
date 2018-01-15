ddfc39562448c5c78dca627ccce1e699b2a3c919
hreskonorbert <norberthresko@gmail.com>
2018-01-15 21:14:41 +0100

    wrote the 'getters' and 'setters' for all guitar classes

--- ElectricGuitar.java
+++ ElectricGuitar.java
@@ -1,12 +1,34 @@
 public class ElectricGuitar extends Guitar{
-    private boolean distorted;
+    private boolean isDistorted;
     private String effect;
     AMP amplifier;
 
-    public ElectricGuitar(String manufacturer,String bodyType,String color,int volume,String tuning,boolean distorted,String effect,AMP amplifier){
+    public ElectricGuitar(String manufacturer,String bodyType,String color,int volume,String tuning,boolean isDistorted,String effect,AMP amplifier){
         super(manufacturer, bodyType, color, volume, tuning);
-        this.distorted=distorted;
+        this.isDistorted=isDistorted;
         this.effect=effect;
+        this.amplifier=amplifier;
+    }
+
+    //getters for attributes
+    public boolean getIsDistorted(){
+        return isDistorted;
+    }
+    public String getEffect(){
+        return effect;
+    }
+    public AMP getAmplifier(){
+        return amplifier;
+    }
+
+    //setters for attributes
+    public void setIsDistorted(boolean isDistorted){
+        this.isDistorted=isDistorted;
+    }
+    public void setEffect(String effect){
+        this.effect=effect;
+    }
+    public void setAmplifier(){
         this.amplifier=amplifier;
     }
 }
--- Guitar.java
+++ Guitar.java
@@ -13,6 +13,40 @@ public class Guitar{
         this.volume=volume;
         this.tuning=tuning;
     }
+    // getters for attributes
+    public String getManufacturer(){
+        return manufacturer;
+    }
+    public String getBodyType(){
+        return bodyType;
+    }
+    public String getColor(){
+        return color;
+    }
+    public int getVolume(){
+        return volume;
+    }
+    public String getTuning(){
+        return tuning;
+    }
+
+    //setters for attributes
+    public void setManufacturer(String manufacturer){
+        this.manufacturer=manufacturer;
+    }
+    public void setBodyType(String bodyType){
+        this.bodyType=bodyType;
+    }
+    public void setColor(String color){
+        this.color=color;
+    }
+    public void setVolume(int volume){
+        this.volume=volume;
+    }
+    public void setTuning(String tunin){
+        this.tuning=tuning;
+    }
+
 
 
 }
