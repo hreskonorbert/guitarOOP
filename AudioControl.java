b671149546839131527238a3efafcd597946d289
hreskonorbert <norberthresko@gmail.com>
2018-01-29 11:58:59 +0100

    Implemented sound interface checking

--- AudioControl.java
+++ AudioControl.java
@@ -11,30 +11,40 @@ public class AudioControl{
     
     public static void playAudio(String url,float volume){
         Mixer.Info[] mixInfo = AudioSystem.getMixerInfo();
-        
-        mixer = AudioSystem.getMixer(mixInfo[0]);
-        DataLine.Info dataInfo = new DataLine.Info(Clip.class, null);
-        try{
-            clip = (Clip)mixer.getLine(dataInfo);
-        }catch(LineUnavailableException e){
-            e.printStackTrace();
-        }
+        boolean isOk=false;
+        int counter =0;
+        while(isOk==false){
+            try{
 
-        try{
-            URL soundUrl = Main.class.getResource(url);
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundUrl);
-            clip.open(audioStream);
-        }catch(LineUnavailableException e){
-            e.printStackTrace();
-        }catch(UnsupportedAudioFileException e){
-            e.printStackTrace();
-        }catch(IOException e){
-            e.printStackTrace();
+            mixer = AudioSystem.getMixer(mixInfo[counter]);
+            DataLine.Info dataInfo = new DataLine.Info(Clip.class, null);
+            try{
+                clip = (Clip)mixer.getLine(dataInfo);
+            }catch(LineUnavailableException e){
+                e.printStackTrace();
+            }
+    
+            try{
+                URL soundUrl = Main.class.getResource(url);
+                AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundUrl);
+                clip.open(audioStream);
+            }catch(LineUnavailableException e){
+                e.printStackTrace();
+            }catch(UnsupportedAudioFileException e){
+                e.printStackTrace();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(volume);
+            isOk=true;
+            clip.start();
+        }catch(Exception e){
+            counter++;
         }
-        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-        gainControl.setValue(volume);
-
-        clip.start();
+    }
         
+
+                
     }
 } 
